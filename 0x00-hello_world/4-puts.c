1eb3af0fdeea6720fa634f8a4538d7af8f4e85ca
John <jonjoro@live.com>
2023-09-22 23:22:19 +0000

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
-#include<stdio.h>
-/** 
- * main - Entry point
- *
- * Return:Always 0 (success) 
- */
-int main(void) 
-{
+#include <stdio.h>
+ /** 
+  * main - Entry point
+  *
+  * Return:Always 0 (success) 
+  *
+  * Return: 0
+  */
+
+ int main(void) 
+ {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-} 	
+ }   	
